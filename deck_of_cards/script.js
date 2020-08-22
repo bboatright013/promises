8543a6204c97c43cca6aeb2bc02fce8e0cc08e18
Brandon Boatright <bboatright013@gmail.com>
2020-08-22 12:34:48 -0400

    changed promises top asynchronous

--- deck_of_cards/script.js
+++ deck_of_cards/script.js
@@ -1,52 +1,91 @@
 const $get_card = $(".get-deck button");
 const $cards = $(".cards");
+// promises
+// let deck_id;
+// $( document ).ready(get_a_deck().then(function(data){
+//     deck_id= data.data.deck_id;
+//     console.log(deck_id);
+// })
+// .catch(function(data){
+//     console.log(data);
+// }));
 
-let deck_id;
-$( document ).ready(get_a_deck().then(function(data){
-    deck_id= data.data.deck_id;
-    console.log(deck_id);
-})
-.catch(function(data){
-    console.log(data);
-}));
-
-function get_a_deck(){
-    return new Promise((resolve, reject) => {
-        const result = axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-        console.log(result);
-        resolve(result);
-        reject(result);
-    })
-}
-
-$get_card.on('click', function(e){
-    e.preventDefault();
-    draw_card(deck_id, 1).then(function(data){
-        console.log(`${data.data.cards[0].value} of ${data.data.cards[0].suit}`);
-        $cards.append(post_card(data.data.cards[0].image));
-    }).catch(error => console.log(error));
-});
+// function get_a_deck(){
+//     return new Promise((resolve, reject) => {
+//         const result = axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+//         console.log(result);
+//         resolve(result);
+//         reject(result);
+//     })
+// }
 
 // $get_card.on('click', function(e){
 //     e.preventDefault();
-//     draw_card(deck_id, 2).then(function(data){
-//         console.log(`${data.data.cards[0].value} of ${data.data.cards[0].suit} & ${data.data.cards[1].value} of ${data.data.cards[1].suit}`);
+//     draw_card(deck_id, 1).then(function(data){
+//         console.log(`${data.data.cards[0].value} of ${data.data.cards[0].suit}`);
+//         $cards.append(post_card(data.data.cards[0].image));
 //     }).catch(error => console.log(error));
 // });
 
-function draw_card(deck_id, num){
-    return new Promise((resolve, reject) => {
-        const result = axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${num}`);
-        console.log(result);
-        resolve(result);
-        reject(result);
-    })
-}
+// // $get_card.on('click', function(e){
+// //     e.preventDefault();
+// //     draw_card(deck_id, 2).then(function(data){
+// //         console.log(`${data.data.cards[0].value} of ${data.data.cards[0].suit} & ${data.data.cards[1].value} of ${data.data.cards[1].suit}`);
+// //     }).catch(error => console.log(error));
+// // });
 
-function post_card(url){
-    let html = 
-    `
-    <img src='${url}'>
-    `;
-    return html;
+// function draw_card(deck_id, num){
+//     return new Promise((resolve, reject) => {
+//         const result = axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${num}`);
+//         console.log(result);
+//         resolve(result);
+//         reject(result);
+//     })
+// }
+
+// function post_card(url){
+//     let html = 
+//     `
+//     <img src='${url}'>
+//     `;
+//     return html;
+// }
+// Asynchronous
+//deck class to handle the deck-id card draw and html build of a card
+class Deck {
+    constructor(id) {
+        this.id = id;
+    }
+    async draw_card(){
+        const result = await axios.get(`https://deckofcardsapi.com/api/deck/${this.id}/draw/?count=1`);
+        return result;
+    }
+    post_card(url){
+        let html = 
+        `
+        <img src='${url}'>
+        `;
+        return html;
+    }
 }
+// function to initialize the deck of cards since we cant await in a constructor
+async function get_a_deck(){
+    try{
+        const result = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+        let id = result.data.deck_id;
+        deck = new Deck(id);
+    }catch(e){
+            console.log(e)
+        }
+}
+//gets the new deck on page load
+$( document ).ready(get_a_deck());
+
+// user event to trigger card draw
+$get_card.on('click', async function(e){
+    e.preventDefault();
+    let card = await deck.draw_card();
+    console.log(`${card.data.cards[0].value} of ${card.data.cards[0].suit}`);
+    $cards.append(deck.post_card(`${card.data.cards[0].image}`));
+});
+
--- promise_unit/script.js
+++ promise_unit/script.js
@@ -2,33 +2,58 @@ const $number_form_button = $(".number-form button");
 const $favorite_number = $("#favorite-number");
 const $number_box = $(".number-box");
 
-let numPromises = [];
+//promises
+// let numPromises = [];
 
-$number_form_button.on('click', function(e){
+// $number_form_button.on('click', function(e){
+//     e.preventDefault();
+//     let fave_num = $favorite_number[0].value;
+//     for(let i = 0; i < 5; i++){
+//         let res = get_number(parseInt(fave_num));
+//         numPromises.push(res);
+//     }
+//     Promise.all(numPromises)
+//     .then(numArr => (
+//         numArr.forEach(data => $number_box.append(add_number_fact(data.data)))
+//       ))
+//     .catch(error => console.log(error))
+
+// })
+
+
+// function get_number(number){
+//     return new Promise((resolve, reject) => {
+//         let response = axios.get(`http://numbersapi.com/${number}?JSON`);
+//         resolve(response);
+//         reject(response);
+//     });
+// }
+
+
+
+// function add_number_fact(data){
+//     let html = 
+//     `
+//     <p class="number-fact">${data}</p>
+//     `;
+//     return html
+// }
+
+//asynchronous
+
+$number_form_button.on('click', async function(e){
     e.preventDefault();
     let fave_num = $favorite_number[0].value;
-    for(let i = 0; i < 5; i++){
-        let res = get_number(parseInt(fave_num));
-        numPromises.push(res);
-    }
-    Promise.all(numPromises)
-    .then(numArr => (
-        numArr.forEach(data => $number_box.append(add_number_fact(data.data)))
-      ))
-    .catch(error => console.log(error))
+    let fact = await get_number(fave_num);
+    $number_box.append(add_number_fact(fact.data));
 
 })
 
-
-function get_number(number){
-    return new Promise((resolve, reject) => {
-        let response = axios.get(`http://numbersapi.com/${number}?JSON`);
-        resolve(response);
-        reject(response);
-    });
-}
-
-
+async function get_number(number){
+    let response = await axios.get(`http://numbersapi.com/${number}?JSON`);
+    console.log(response);
+    return response;
+};
 
 function add_number_fact(data){
     let html = 
